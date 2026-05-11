7f092c195c4c4c42542a4fd6abc5c5459dc90f90
gabi-abs <gabriellasantana2201@gmail.com>
2026-05-11 18:55:10 -0300

    feat: atualização para testes via swagger em prod

--- src/server.js
+++ src/server.js
@@ -11,12 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const swaggerDocument = YAML.load('./swagger.yaml');
+if (process.env.SERVER_URL) {
+  swaggerDocument.servers = [{ url: `${process.env.SERVER_URL}/api`, description: 'Servidor atual' }];
+}
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api', routes);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
   console.log(`NoticeService rodando na porta ${PORT}`);
   console.log(`Documentação Swagger: http://localhost:${PORT}/api-docs`);
